5bcb95b6dd6416e67276b7b93062e8fb8486b11f
ro-ze1106 <roze56477@gmail.com>
2022-06-11 19:32:16 +0900

    interface

---
+++ generics.js
@@ -0,0 +1,8 @@
+"use strict";
+function copy(value) {
+    return value;
+}
+console.log(copy({ name: 'user' }));
+function huma() {
+    console.log('u');
+}
---
+++ interface.js
@@ -0,0 +1,5 @@
+"use strict";
+const human = {
+    name: 'Quill',
+    age: 38
+};
